2dd98169addd29b85b3c422bac76a45c797a5fbd
gaoooon <gaon9749@gmail.com>
2023-08-22 16:51:11 +0900

    Update Css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
-import Input from "./Input";
-import Weather from "./Weather";
-import { weather } from "./model/data";
+import Input from "./Input/index";
+import Weather from "./Weather/index";
+import { WeatherForm } from "./model/data";
+import { GlobalStyle } from "./resetCss";
+import { Style } from "./style";
 
 const App: React.FC = () => {
   //함수형 컴포넌트타입으로 지정해줌
-  const [cityWeather, setCityWeather] = useState<weather>({
+  const [cityWeather, setCityWeather] = useState<WeatherForm>({
     city: "",
     weather: "",
     temp: 0,
   });
   return (
-    <div>
-      <Input setter={setCityWeather} />
-      <Weather cityWeather={cityWeather} />
-    </div>
+    <>
+      <Style />
+      <GlobalStyle />
+      <div>
+        <Input setter={setCityWeather} />
+        <Weather cityWeather={cityWeather} />
+      </div>
+    </>
   );
 };
 
---
+++ src/style.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Style = createGlobalStyle`
+body {
+  background-color: #91C8E4;
+}
+
+body > div {
+  flex-direction:column;
+  align-items: center;  
+  display: flex;
+}
+`;
