2b0dcff099ed40468391202fb2a09114f3586c62
gaoooon <gaon9749@gmail.com>
2023-08-22 16:50:07 +0900

    Update Weather

---
+++ src/Weather/index.tsx
@@ -0,0 +1,44 @@
+import { WeatherForm, WeatherType } from "../model/data";
+import { WeatherContainer } from "./style";
+
+const weatherTypes: WeatherType = {
+  Clear: "맑음",
+  "Partly Cloudy": "구름 조금",
+  Clouds: "흐림",
+  Overcast: "구름 많음",
+  Rain: "비",
+  Drizzle: "이슬비",
+  Showers: "소나기",
+  Thunderstorms: "천둥번개",
+  Snow: "눈",
+  Fog: "안개",
+  Hail: "우박",
+  Mist: "안개",
+  Smoke: "연기",
+  Dust: "먼지",
+  Sand: "모래",
+  Ash: "화산재",
+  Squalls: "돌풍",
+  Tornado: "토네이도",
+};
+
+interface props {
+  cityWeather: WeatherForm;
+}
+
+const Weather: React.FC<props> = ({ cityWeather }) => {
+  console.log(cityWeather.weather);
+  return (
+    <>
+      {cityWeather.temp !== 0 ? (
+        <WeatherContainer>
+          <div>지역:{cityWeather.city}</div>{" "}
+          <div>날씨:{weatherTypes[cityWeather.weather]}</div>
+          <div>온도:{cityWeather.temp}</div>
+        </WeatherContainer>
+      ) : null}
+    </>
+  );
+};
+
+export default Weather;
---
+++ src/Weather/style.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const WeatherContainer = styled.div`
+  @font-face {
+    font-family: "HakgyoansimBareondotumB";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-2@1.0/HakgyoansimBareondotumB.woff2")
+      format("woff2");
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  border-radius: 30px 30px 30px 30px;
+  background-color: #f6f4eb;
+  font-size: 30px;
+  font-family: "HakgyoansimBareondotumB";
+  text-align: center;
+  padding: 30px 90px;
+  box-shadow: 0px 0px 5px 3px rgb(1, 1, 1, 0.5);
+
+  div {
+    background-color: #91c8e4;
+    padding: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border-radius: 20px;
+  }
+`;
+
+//@font-face {
+//     font-family: 'PuradakGentleGothicR';
+//     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.1/PuradakGentleGothicR.woff2') format('woff2');
+//     font-weight: normal;
+//     font-style: normal;
+// }
